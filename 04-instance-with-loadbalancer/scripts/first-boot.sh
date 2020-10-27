3fcd12a9dfca21ed3f9b15b3e4fb1ac4747c43e7
thisismsreddy <thisismsreddy@gmail.com>
2020-10-27 10:57:58 +0300

    Update first-boot.sh

--- 04-instance-with-loadbalancer/scripts/first-boot.sh
+++ 04-instance-with-loadbalancer/scripts/first-boot.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
-ping -c 8 google.com
-sudo apt install nginx -y
-sudo systemctl enable nginx
 echo "this is $HOSTNAME" >> /var/www/html/index.html
